07172b36c9f65081fafbc40cc9d8520085cc884b
Kristopher Overholt <koverholt@gmail.com>
2022-12-08 17:24:26 -0600

    Update Terraform

--- agent.tf
+++ agent.tf
@@ -1,141 +1,67 @@
 resource "google_dialogflow_cx_agent" "agent" {
-  display_name               = "dialogflowcx-agent"
+  display_name               = "dialogflow-cx-agent"
   location                   = var.region
   default_language_code      = "en"
   time_zone                  = "America/Chicago"
-  description                = "Example description"
-  enable_stackdriver_logging = true
-  enable_spell_correction    = true
-    speech_to_text_settings {
-        enable_speech_adaptation = true
-    }
 }
 
 resource "google_dialogflow_cx_flow" "default_start_flow" {
   parent       = google_dialogflow_cx_agent.agent.id
-  display_name = "Default Start Flow 2"
+  display_name = "Default Start Flow"
   description  = "A start flow created along with the agent"
 
   nlu_settings {
-        classification_threshold = 0.3
-        model_type               = "MODEL_TYPE_STANDARD"
+    classification_threshold = 0.3
+    model_type               = "MODEL_TYPE_STANDARD"
     }
-
-  transition_routes {
-    intent = "${google_dialogflow_cx_agent.agent.id}/intents/00000000-0000-0000-0000-000000000000"
-    trigger_fulfillment {
-      return_partial_responses = false
-        messages {
-          text {
-            text  = ["Hello, this is a shirt ordering virtual agent. How can I help you?"]
-          }
-        }
-    }
-  }
-
-  event_handlers {
-      event = "sys.no-match-default"
-      trigger_fulfillment {
-        return_partial_responses = false
-        messages {
-          text {
-            text  = ["Sorry, could you say that again?"]
-          }
-        }
-      }
-  }
-
-  event_handlers {
-    event = "sys.no-input-default"
-    trigger_fulfillment {
-          return_partial_responses = false
-          messages {
-              text {
-                  text  = ["One more time?"]
-              }
-          }
-      }
-  }
 }
 
-resource "google_dialogflow_cx_intent" "store_location" {
-  parent       = google_dialogflow_cx_agent.agent.id
-  display_name = "store.location"
-  priority     = 1
-  description  = "Intent example"
+# resource "google_dialogflow_cx_intent" "default_welcome_intent" {
+#   parent       = google_dialogflow_cx_agent.agent.id
+#   display_name = "Example"
+# }
 
-  training_phrases {
-     parts {
-         text = "Where is the store?"
-     }
-     repeat_count = 1
-  }
+# resource "google_dialogflow_cx_intent" "store_hours" {
+#   parent       = google_dialogflow_cx_agent.agent.id
+#   display_name = "store.hours"
+# }
 
-  training_phrases {
-     parts {
-         text = "Directions"
-     }
-     repeat_count = 1
-  }
+# resource "google_dialogflow_cx_intent" "order_new" {
+#   parent       = google_dialogflow_cx_agent.agent.id
+#   display_name = "order.new"
+# }
 
-  training_phrases {
-     parts {
-         text = "Tell me the address"
-     }
-     repeat_count = 1
-  }
+# resource "google_dialogflow_cx_intent" "store_location" {
+#   parent       = google_dialogflow_cx_agent.agent.id
+#   display_name = "store.location"
+# }
 
-  training_phrases {
-     parts {
-         text = "Where do I pick up my order?"
-     }
-     repeat_count = 1
-  }
+# resource "google_dialogflow_cx_entity_type" "size" {
+#   parent       = google_dialogflow_cx_agent.agent.id
+#   display_name = "size"
+#   kind         = "KIND_MAP"
+#   entities {
+#     value = "value1"
+#     synonyms = ["synonym1","synonym2"]
+#   }
+# }
 
-  training_phrases {
-     parts {
-         text = "How do I get there?"
-     }
-     repeat_count = 1
-  }
+# resource "google_dialogflow_cx_page" "new_order" {
+#   parent       = google_dialogflow_cx_flow.default_start_flow.id
+#   display_name = "New Order"
+# }
 
-  training_phrases {
-     parts {
-         text = "Where is the store located?"
-     }
-     repeat_count = 1
-  }
+# resource "google_dialogflow_cx_page" "order_confirmation" {
+#   parent       = google_dialogflow_cx_flow.default_start_flow.id
+#   display_name = "Order Confirmation"
+# }
 
-  training_phrases {
-     parts {
-         text = "What street are you on?"
-     }
-     repeat_count = 1
-  }
+# resource "google_dialogflow_cx_page" "store_hours" {
+#   parent       = google_dialogflow_cx_flow.default_start_flow.id
+#   display_name = "Store Hours"
+# }
 
-  training_phrases {
-     parts {
-         text = "What is your address?"
-     }
-     repeat_count = 1
-  }
-
-  training_phrases {
-     parts {
-         text = "How do I get to your store?"
-     }
-     repeat_count = 1
-  }
-
-  training_phrases {
-     parts {
-         text = "Where are you located?"
-     }
-     repeat_count = 1
-  }
-
-  parameters {
-    id          = "param1"
-    entity_type = "projects/-/locations/-/agents/-/entityTypes/sys.date"
-  }
-}
+# resource "google_dialogflow_cx_page" "store_location" {
+#   parent       = google_dialogflow_cx_flow.default_start_flow.id
+#   display_name = "MyPage"
+# }
