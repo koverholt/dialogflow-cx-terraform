9ea1afd0e7a22cb00e8bee1eef7dba00a4b6a4da
Kristopher Overholt <koverholt@gmail.com>
2022-12-09 16:33:02 -0600

    Update

--- agents.tf
+++ agents.tf
@@ -1,6 +1,8 @@
 resource "google_dialogflow_cx_agent" "agent" {
-  display_name          = "dialogflow-cx-agent"
+  display_name          = "dialogflow-cx-agent2"
   location              = var.region
   default_language_code = "en"
   time_zone             = "America/Chicago"
 }
+
+# TODO: Rename agent display name
--- pages.tf
+++ pages.tf
@@ -1,106 +1,108 @@
-resource "google_dialogflow_cx_page" "new_order" {
-  parent       = google_dialogflow_cx_flow.default_start_flow.id
-  display_name = "New Order"
+# TODO: Change these absolute references to point to newly created resources
 
-  form {
-    parameters {
-      display_name = "color"
-      entity_type  = "projects/-/locations/-/agents/-/entityTypes/sys.color"
-      is_list      = false
-      redact       = false
-      required     = true
+# resource "google_dialogflow_cx_page" "new_order" {
+#   parent       = google_dialogflow_cx_flow.default_start_flow.id
+#   display_name = "New Order"
 
-      fill_behavior {
-        initial_prompt_fulfillment {
-          return_partial_responses = false
+#   form {
+#     parameters {
+#       display_name = "color"
+#       entity_type  = "projects/-/locations/-/agents/-/entityTypes/sys.color"
+#       is_list      = false
+#       redact       = false
+#       required     = true
 
-          messages {
-            text {
-              text = [
-                "What color would you like?",
-              ]
-            }
-          }
-        }
-      }
-    }
-    parameters {
-      display_name = "size"
-      entity_type  = "projects/koverholt-dev/locations/us-central1/agents/73696d8a-b312-460e-a88a-3fc00827ed69/entityTypes/9cecd920-bc46-4edf-99b2-2c7059d8b92b"
-      is_list      = false
-      redact       = false
-      required     = true
+#       fill_behavior {
+#         initial_prompt_fulfillment {
+#           return_partial_responses = false
 
-      fill_behavior {
-        initial_prompt_fulfillment {
-          return_partial_responses = false
+#           messages {
+#             text {
+#               text = [
+#                 "What color would you like?",
+#               ]
+#             }
+#           }
+#         }
+#       }
+#     }
+#     parameters {
+#       display_name = "size"
+#       entity_type  = "projects/koverholt-dev/locations/us-central1/agents/73696d8a-b312-460e-a88a-3fc00827ed69/entityTypes/9cecd920-bc46-4edf-99b2-2c7059d8b92b"
+#       is_list      = false
+#       redact       = false
+#       required     = true
 
-          messages {
-            text {
-              text = [
-                "What size do you want?",
-              ]
-            }
-          }
-        }
-      }
-    }
-  }
+#       fill_behavior {
+#         initial_prompt_fulfillment {
+#           return_partial_responses = false
 
-  transition_routes {
-    condition   = "$page.params.status = \"FINAL\""
-    target_page = "projects/koverholt-dev/locations/us-central1/agents/73696d8a-b312-460e-a88a-3fc00827ed69/flows/00000000-0000-0000-0000-000000000000/pages/ec0be6d8-6c29-4233-a290-4f60c3fe7880"
+#           messages {
+#             text {
+#               text = [
+#                 "What size do you want?",
+#               ]
+#             }
+#           }
+#         }
+#       }
+#     }
+#   }
 
-    trigger_fulfillment {
-      return_partial_responses = false
+#   transition_routes {
+#     condition   = "$page.params.status = \"FINAL\""
+#     target_page = "projects/koverholt-dev/locations/us-central1/agents/73696d8a-b312-460e-a88a-3fc00827ed69/flows/00000000-0000-0000-0000-000000000000/pages/ec0be6d8-6c29-4233-a290-4f60c3fe7880"
 
-      messages {
-        text {
-          text = [
-            "You have selected a $session.params.size, $session.params.color shirt.",
-          ]
-        }
-      }
-    }
-  }
-  transition_routes {
-    condition = "true"
+#     trigger_fulfillment {
+#       return_partial_responses = false
 
-    trigger_fulfillment {
-      return_partial_responses = false
+#       messages {
+#         text {
+#           text = [
+#             "You have selected a $session.params.size, $session.params.color shirt.",
+#           ]
+#         }
+#       }
+#     }
+#   }
+#   transition_routes {
+#     condition = "true"
 
-      messages {
-        text {
-          text = [
-            "I'd like to collect a bit more information from you.",
-          ]
-        }
-      }
-    }
-  }
-}
+#     trigger_fulfillment {
+#       return_partial_responses = false
 
-resource "google_dialogflow_cx_page" "order_confirmation" {
-  parent       = google_dialogflow_cx_flow.default_start_flow.id
-  display_name = "Order Confirmation"
+#       messages {
+#         text {
+#           text = [
+#             "I'd like to collect a bit more information from you.",
+#           ]
+#         }
+#       }
+#     }
+#   }
+# }
 
-  entry_fulfillment {
-    return_partial_responses = false
+# resource "google_dialogflow_cx_page" "order_confirmation" {
+#   parent       = google_dialogflow_cx_flow.default_start_flow.id
+#   display_name = "Order Confirmation"
 
-    messages {
-      text {
-        text = [
-          "You can pick up your order for a $session.params.size $session.params.color shirt in 7 to 10 business days. Goodbye.",
-        ]
-      }
-    }
-  }
+#   entry_fulfillment {
+#     return_partial_responses = false
 
-  transition_routes {
-    condition   = "true"
-    target_page = "projects/koverholt-dev/locations/us-central1/agents/73696d8a-b312-460e-a88a-3fc00827ed69/flows/00000000-0000-0000-0000-000000000000/pages/END_SESSION"
-  }
-}
+#     messages {
+#       text {
+#         text = [
+#           "You can pick up your order for a $session.params.size $session.params.color shirt in 7 to 10 business days. Goodbye.",
+#         ]
+#       }
+#     }
+#   }
+
+#   transition_routes {
+#     condition   = "true"
+#     target_page = "projects/koverholt-dev/locations/us-central1/agents/73696d8a-b312-460e-a88a-3fc00827ed69/flows/00000000-0000-0000-0000-000000000000/pages/END_SESSION"
+#   }
+# }
 
 resource "google_dialogflow_cx_page" "store_hours" {
   parent       = google_dialogflow_cx_flow.default_start_flow.id
