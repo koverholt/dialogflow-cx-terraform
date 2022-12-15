30232226155b203f91a1e9a491bc3661ed50a0ba
Kristopher Overholt <koverholt@gmail.com>
2022-12-15 16:23:26 -0600

    Update references throughout

--- flows.tf
+++ flows.tf
@@ -10,33 +10,34 @@ resource "google_dialogflow_cx_flow" "default_start_flow" {
     model_type               = "MODEL_TYPE_STANDARD"
   }
 
-# TODO: Change these absolute references to point to newly created resources
+  transition_routes {
+    intent = google_dialogflow_cx_intent.default_welcome_intent.id
 
-#   transition_routes {
-#     intent = "projects/koverholt-dev/locations/us-central1/agents/73696d8a-b312-460e-a88a-3fc00827ed69/intents/00000000-0000-0000-0000-000000000000"
+    trigger_fulfillment {
+      return_partial_responses = false
+      messages {
+        text {
+          text = ["Hello, this is a shirt ordering virtual agent. How can I help you?"]
+        }
+      }
+    }
+  }
 
-#     trigger_fulfillment {
-#       return_partial_responses = false
-#       messages {
-#         text {
-#           text = ["Hello, this is a shirt ordering virtual agent. How can I help you?"]
-#         }
-#       }
-#     }
-#   }
+  # transition_routes {
+  #   intent      = google_dialogflow_cx_intent.store_location.id
+  #   target_page = google_dialogflow_cx_page.store_location.id
+  # }
 
-#   transition_routes {
-#     intent      = "projects/koverholt-dev/locations/us-central1/agents/73696d8a-b312-460e-a88a-3fc00827ed69/intents/7608701f-a8f5-4519-9b05-97ec2094c634"
-#     target_page = "projects/koverholt-dev/locations/us-central1/agents/73696d8a-b312-460e-a88a-3fc00827ed69/flows/00000000-0000-0000-0000-000000000000/pages/e736abf0-bc00-4d65-936b-f56033d12433"
-#   }
+  # transition_routes {
+  #   intent      = google_dialogflow_cx_intent.store_hours.id
+  #   target_page = google_dialogflow_cx_page.store_hours.id
+  # }
 
-#   transition_routes {
-#     intent      = "projects/koverholt-dev/locations/us-central1/agents/73696d8a-b312-460e-a88a-3fc00827ed69/intents/327ed726-043e-41b7-af1f-f34a10a0f1e6"
-#     target_page = "projects/koverholt-dev/locations/us-central1/agents/73696d8a-b312-460e-a88a-3fc00827ed69/flows/00000000-0000-0000-0000-000000000000/pages/bc1f8bf5-8947-4e6e-bfcc-8135da845441"
-#   }
+  # transition_routes {
+  #   intent      = google_dialogflow_cx_intent.order_new.id
+  #   target_page = google_dialogflow_cx_page.new_order.id
+  # }
 
-#   transition_routes {
-#     intent      = "projects/koverholt-dev/locations/us-central1/agents/73696d8a-b312-460e-a88a-3fc00827ed69/intents/551e2b01-f2bf-49da-8ff4-92adf139b736"
-#     target_page = "projects/koverholt-dev/locations/us-central1/agents/73696d8a-b312-460e-a88a-3fc00827ed69/flows/00000000-0000-0000-0000-000000000000/pages/8806f1fb-c45e-43ba-ac16-47a668ddc9eb"
-#   }
+  # TODO: Fix these transition routes, avoid cyclic graph
+
 }
--- pages.tf
+++ pages.tf
@@ -1,108 +1,20 @@
-# TODO: Change these absolute references to point to newly created resources
 
-# resource "google_dialogflow_cx_page" "new_order" {
-#   parent       = google_dialogflow_cx_flow.default_start_flow.id
-#   display_name = "New Order"
+resource "google_dialogflow_cx_page" "store_location" {
+  parent       = google_dialogflow_cx_flow.default_start_flow.id
+  display_name = "Store Location"
 
-#   form {
-#     parameters {
-#       display_name = "color"
-#       entity_type  = "projects/-/locations/-/agents/-/entityTypes/sys.color"
-#       is_list      = false
-#       redact       = false
-#       required     = true
+  entry_fulfillment {
+    return_partial_responses = false
 
-#       fill_behavior {
-#         initial_prompt_fulfillment {
-#           return_partial_responses = false
-
-#           messages {
-#             text {
-#               text = [
-#                 "What color would you like?",
-#               ]
-#             }
-#           }
-#         }
-#       }
-#     }
-#     parameters {
-#       display_name = "size"
-#       entity_type  = "projects/koverholt-dev/locations/us-central1/agents/73696d8a-b312-460e-a88a-3fc00827ed69/entityTypes/9cecd920-bc46-4edf-99b2-2c7059d8b92b"
-#       is_list      = false
-#       redact       = false
-#       required     = true
-
-#       fill_behavior {
-#         initial_prompt_fulfillment {
-#           return_partial_responses = false
-
-#           messages {
-#             text {
-#               text = [
-#                 "What size do you want?",
-#               ]
-#             }
-#           }
-#         }
-#       }
-#     }
-#   }
-
-#   transition_routes {
-#     condition   = "$page.params.status = \"FINAL\""
-#     target_page = "projects/koverholt-dev/locations/us-central1/agents/73696d8a-b312-460e-a88a-3fc00827ed69/flows/00000000-0000-0000-0000-000000000000/pages/ec0be6d8-6c29-4233-a290-4f60c3fe7880"
-
-#     trigger_fulfillment {
-#       return_partial_responses = false
-
-#       messages {
-#         text {
-#           text = [
-#             "You have selected a $session.params.size, $session.params.color shirt.",
-#           ]
-#         }
-#       }
-#     }
-#   }
-#   transition_routes {
-#     condition = "true"
-
-#     trigger_fulfillment {
-#       return_partial_responses = false
-
-#       messages {
-#         text {
-#           text = [
-#             "I'd like to collect a bit more information from you.",
-#           ]
-#         }
-#       }
-#     }
-#   }
-# }
-
-# resource "google_dialogflow_cx_page" "order_confirmation" {
-#   parent       = google_dialogflow_cx_flow.default_start_flow.id
-#   display_name = "Order Confirmation"
-
-#   entry_fulfillment {
-#     return_partial_responses = false
-
-#     messages {
-#       text {
-#         text = [
-#           "You can pick up your order for a $session.params.size $session.params.color shirt in 7 to 10 business days. Goodbye.",
-#         ]
-#       }
-#     }
-#   }
-
-#   transition_routes {
-#     condition   = "true"
-#     target_page = "projects/koverholt-dev/locations/us-central1/agents/73696d8a-b312-460e-a88a-3fc00827ed69/flows/00000000-0000-0000-0000-000000000000/pages/END_SESSION"
-#   }
-# }
+    messages {
+      text {
+        text = [
+          "Our store is located at 1007 Mountain Drive, Gotham City, NJ.",
+        ]
+      }
+    }
+  }
+}
 
 resource "google_dialogflow_cx_page" "store_hours" {
   parent       = google_dialogflow_cx_flow.default_start_flow.id
@@ -121,9 +33,91 @@ resource "google_dialogflow_cx_page" "store_hours" {
   }
 }
 
-resource "google_dialogflow_cx_page" "store_location" {
+resource "google_dialogflow_cx_page" "new_order" {
   parent       = google_dialogflow_cx_flow.default_start_flow.id
-  display_name = "Store Location"
+  display_name = "New Order"
+
+  form {
+    parameters {
+      display_name = "color"
+      entity_type  = "projects/-/locations/-/agents/-/entityTypes/sys.color"
+      is_list      = false
+      redact       = false
+      required     = true
+
+      fill_behavior {
+        initial_prompt_fulfillment {
+          return_partial_responses = false
+
+          messages {
+            text {
+              text = [
+                "What color would you like?",
+              ]
+            }
+          }
+        }
+      }
+    }
+    parameters {
+      display_name = "size"
+      entity_type  = google_dialogflow_cx_entity_type.size.id
+      is_list      = false
+      redact       = false
+      required     = true
+
+      fill_behavior {
+        initial_prompt_fulfillment {
+          return_partial_responses = false
+
+          messages {
+            text {
+              text = [
+                "What size do you want?",
+              ]
+            }
+          }
+        }
+      }
+    }
+  }
+
+  transition_routes {
+    condition   = "$page.params.status = \"FINAL\""
+    target_page = google_dialogflow_cx_page.order_confirmation.id
+
+    trigger_fulfillment {
+      return_partial_responses = false
+
+      messages {
+        text {
+          text = [
+            "You have selected a $session.params.size, $session.params.color shirt.",
+          ]
+        }
+      }
+    }
+  }
+  transition_routes {
+    condition = "true"
+
+    trigger_fulfillment {
+      return_partial_responses = false
+
+      messages {
+        text {
+          text = [
+            "I'd like to collect a bit more information from you.",
+          ]
+        }
+      }
+    }
+  }
+}
+
+resource "google_dialogflow_cx_page" "order_confirmation" {
+  parent       = google_dialogflow_cx_flow.default_start_flow.id
+  display_name = "Order Confirmation"
 
   entry_fulfillment {
     return_partial_responses = false
@@ -131,9 +125,17 @@ resource "google_dialogflow_cx_page" "store_location" {
     messages {
       text {
         text = [
-          "Our store is located at 1007 Mountain Drive, Gotham City, NJ.",
+          "You can pick up your order for a $session.params.size $session.params.color shirt in 7 to 10 business days. Goodbye.",
         ]
       }
     }
   }
+
+  # transition_routes {
+  #   condition   = "true"
+  #   target_page = "projects/-/locations/-/agents/-/flows/00000000-0000-0000-0000-000000000000/pages/END_SESSION"
+  # }
+
+  # TODO: Fix this transition route to be managed rather than a static reference
+
 }
